df74367afc0271e2bfca648cb7f4c7ac7a6f9254
Sean Earle <sean.r.earle@gmail.com>
2020-04-17 20:33:30 +0930

    Update the docker

--- blob-test/run_test.sh
+++ blob-test/run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -exuo pipefail
 
-docker-compose build
+docker-compose build --pull
 docker-compose up -d
 docker-compose exec app rails db:create db:schema:load
 
 bundle install
-bundle exec rspec test_suite.rb
+bundle exec rspec test_suite.rb --format doc
 
 docker-compose down
