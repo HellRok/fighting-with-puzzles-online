bd811bce17c43d9e33a4d10e2a1c9ac279a0a4fa
Sean Earle <sean.r.earle@gmail.com>
2020-05-13 21:42:26 +0930

    Update specs for all smasher

--- blob-test/test_suite.rb
+++ blob-test/test_suite.rb
@@ -3,6 +3,7 @@
 # https://stackoverflow.com/a/38861839
 
 require 'bundler'
+require 'open-uri'
 
 Bundler.require
 
@@ -15,6 +16,8 @@ Capybara.default_driver = ENV.fetch('CAPYBARA_DRIVER', :selenium_chrome_headless
 Capybara.app_host = 'http://localhost:9999'
 Capybara.current_session.current_window.resize_to(1280, 800)
 Capybara.save_path = './tmp'
+
+bad_strings = JSON.parse(URI.open('https://raw.githubusercontent.com/minimaxir/big-list-of-naughty-strings/master/blns.json').read)
 
 describe 'First time banner', type: :feature do
   before do
@@ -33,6 +36,12 @@ describe 'First time banner', type: :feature do
   end
 end
 
+USERNAME = bad_strings.sample
+PASSWORD = bad_strings.sample
+
+puts "Username: #{USERNAME}"
+puts "Password: #{PASSWORD}"
+
 describe 'User registration and sessions', type: :feature do
   before do
     visit '/'
@@ -40,18 +49,18 @@ describe 'User registration and sessions', type: :feature do
 
   it 'lets you create a user' do
     click_link 'Register'
-    fill_in 'Username', with: 'TestUser'
-    fill_in 'Password', with: 'Password123'
-    fill_in 'Password Confirmation', with: 'Password123'
+    fill_in 'Username', with: USERNAME
+    fill_in 'Password', with: PASSWORD
+    fill_in 'Password Confirmation', with: PASSWORD
     click_button 'Submit'
 
-    expect(page).to have_content 'Welcome TestUser!'
+    expect(page).to have_content "Welcome #{USERNAME}!"
   end
 
   it 'has sensible error messages on registration' do
     click_link 'Register'
-    fill_in 'Username', with: 'TestUser'
-    fill_in 'Password', with: 'Password123'
+    fill_in 'Username', with: USERNAME
+    fill_in 'Password', with: PASSWORD
     fill_in 'Password Confirmation', with: 'SomethingElse'
     click_button 'Submit'
 
@@ -61,17 +70,17 @@ describe 'User registration and sessions', type: :feature do
 
   it 'lets you login as the newly created user' do
     click_link 'Login'
-    fill_in 'Username', with: 'TestUser'
-    fill_in 'Password', with: 'Password123'
+    fill_in 'Username', with: USERNAME
+    fill_in 'Password', with: PASSWORD
     click_button 'Submit'
 
-    expect(page).to have_content 'Logged in as TestUser'
+    expect(page).to have_content "Logged in as #{USERNAME}"
   end
 
   it 'lets you logout' do
     click_link 'Login'
-    fill_in 'Username', with: 'TestUser'
-    fill_in 'Password', with: 'Password123'
+    fill_in 'Username', with: USERNAME
+    fill_in 'Password', with: PASSWORD
     click_button 'Submit'
 
     click_link 'Logout'
@@ -107,11 +116,11 @@ describe 'Sprint game mode', type: :feature do
   end
 
   it 'can play the replay' do
-    click_link 'TestUser'
+    click_link USERNAME
     click_link href: /sprint\/replay\/\d/
     sleep 10
     expect(page).to have_content 'Sprint Replay'
-    expect(page).to have_content 'TestUser'
+    expect(page).to have_content USERNAME
     expect(page).to have_content 'Finished'
     expect(page).to have_content 'They took 0:0'
     expect(page).to have_content 'Gems: -4'
@@ -128,64 +137,63 @@ describe 'Survival game mode', type: :feature do
   it 'lets you win' do
     press_key 'q'
     sleep 7.3
-    13.times { press_key 'w' }
+    17.times { press_key 'w' }
     expect(page).to have_content 'Finished'
-    expect(page).to have_content 'You survived 0:07'
-    expect(page).to have_content 'Score: 800'
+    expect(page).to have_content 'You survived 0:0'
+    expect(page).to have_content 'Score: 1,000'
     expect(page).to have_content 'Replay saved'
   end
 
   it 'can play the replay' do
-    click_link 'TestUser'
+    click_link USERNAME
     click_link href: /survival\/replay\/\d/
     sleep 10
     expect(page).to have_content 'Survival Replay'
-    expect(page).to have_content 'TestUser'
+    expect(page).to have_content USERNAME
     expect(page).to have_content 'Finished'
-    expect(page).to have_content 'They survived 0:07'
-    expect(page).to have_content 'Score: 800'
+    expect(page).to have_content 'They survived 0:0'
+    expect(page).to have_content 'Score: 1,000'
   end
 end
 
 describe 'Ultra game mode', type: :feature do
   before do
     login
-    visit '/ultra?seed=123456'
+    visit '/ultra?seed=1588857284522'
     setup_keys
   end
 
   it 'lets you die' do
     press_key 'q'
     sleep 1.3
-    15.times { press_key 'w' }
+    17.times { press_key 'w' }
     expect(page).to have_content 'Oh no, you topped out!'
   end
 
   it 'lets you win' do
     press_key 'q'
-    sleep 1.3
     play_replay('./games/ultra_win.json')
     expect(page).to have_content 'Finished'
-    expect(page).to have_content 'Score: 46,900'
+    expect(page).to have_content 'Score: 57,450'
     expect(page).to have_content 'Replay saved'
   end
 
   it 'can play the replay' do
-    click_link 'TestUser'
+    click_link USERNAME
     click_link href: /ultra\/replay\/\d/
     sleep 183
     expect(page).to have_content 'Ultra Replay'
-    expect(page).to have_content 'TestUser'
+    expect(page).to have_content USERNAME
     expect(page).to have_content 'Finished'
-    expect(page).to have_content 'Their score was 46,900!'
-    expect(page).to have_content 'Score: 46,900'
+    expect(page).to have_content 'Their score was 57,450!'
+    expect(page).to have_content 'Score: 57,450'
   end
 end
 
 def login
   visit '/login'
-  fill_in 'Username', with: 'TestUser'
-  fill_in 'Password', with: 'Password123'
+  fill_in 'Username', with: USERNAME
+  fill_in 'Password', with: PASSWORD
   click_button 'Submit'
 end
 
